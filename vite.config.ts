c85cdadd547d6fa5e134f7bea2ff55196ea7c84c
Vittv <166668150+Vittv@users.noreply.github.com>
2026-06-06 20:20:52 -0300

    add mdx config and ultimate data

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import babel from "@rolldown/plugin-babel";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    { enforce: "pre", ...mdx() },
     react(),
     babel({
       presets: [reactCompilerPreset()],
