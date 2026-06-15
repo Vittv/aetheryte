363d0be3f52b015eb266a32672d463edee29541f
Vittv <166668150+Vittv@users.noreply.github.com>
2026-06-15 14:44:39 -0300

    chore(nprogress): format nprogress component

--- src/components/NavigationProgress.tsx
+++ src/components/NavigationProgress.tsx
@@ -1,6 +1,6 @@
+import NProgress from "nprogress";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
 NProgress.configure({ showSpinner: false, speed: 400, minimum: 0.3 });
@@ -15,4 +15,3 @@ export default function NavigationProgress() {
 
   return null;
 }
-
